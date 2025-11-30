34b0dd76b6b987d9c8050c68ed7aae297486ceeb
Holger Ehrmann <holgerehrmann@gmail.com>
2025-11-30 05:17:29 +0100

    Chore: Update Ruby to 3.3 (#13)
    * Upgrade Ruby version to 3.3.10
    
    Update Dockerfile and Gemfile to use Ruby 3.3.10 instead of 3.2.6, enabling new features such as the Prism parser, improved YJIT, and M:N threading.
    
    * Update Ruby and Rails versions in docs and schema
    
    Bump Ruby version to 3.3.10 in Gemfile.lock and README. Update Rails version reference in db/schema.rb from 7.0 to 7.2 to reflect current framework version.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2025_11_17_155609) do
+ActiveRecord::Schema[7.2].define(version: 2025_11_17_155609) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -527,5 +527,4 @@ ActiveRecord::Schema[7.0].define(version: 2025_11_17_155609) do
     t.datetime "created_at", precision: nil
     t.datetime "updated_at", precision: nil
   end
-
 end
