f0d507f557ad7b4cccedd289137d46b1271b9ea8
McNamara <holgerehrmann@gmail.com>
2025-12-02 20:36:50 +0100

    Update plpgsql extension name in schema
    Changed the enabled extension from 'plpgsql' to 'pg_catalog.plpgsql' in db/schema.rb to match the required database configuration.

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 
 ActiveRecord::Schema[8.1].define(version: 2025_12_02_120000) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension "pg_catalog.plpgsql"
 
   create_table "analyses", id: :serial, force: :cascade do |t|
     t.datetime "created_at", precision: nil
