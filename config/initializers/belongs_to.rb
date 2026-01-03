9a3e3bf30d9cc10a7d104d9e3a7d9951cc497a84
Holger Ehrmann <holgerehrmann@gmail.com>
2026-01-03 05:26:21 -0500

    Deleted workaround

--- config/initializers/belongs_to.rb
+++ config/initializers/belongs_to.rb
@@ -1,27 +1,8 @@
-module ActiveRecord::Associations::Builder
-  class BelongsTo
-    def define_accessors
-      super
-      define_global_id_accessors
-    end
-
-    def define_global_id_accessors
-      mixin.class_eval <<-CODE, __FILE__, __LINE__ + 1
-        def #{name}_global_id
-          association(:#{name}).load_target.try(:global_id)
-        end
-
-        def #{name}_global_id=(id)
-          if id.present?
-            record = ::RecordProperty.find_by(global_id: id).try!(:datum)
-            if record.is_a? association(:#{name}).klass
-              write_attribute(association(:#{name}).reflection.foreign_key, record.id)
-            end
-          else
-            write_attribute(association(:#{name}).reflection.foreign_key, nil)
-          end
-        end
-      CODE
-    end
-  end
-end
+## NOTE: intentionally left blank.
+#
+# This file previously monkeypatched `ActiveRecord::Associations::Builder::BelongsTo`
+# to auto-generate `*_global_id` accessors for every `belongs_to` association.
+#
+# That approach was very invasive and brittle across Rails upgrades.
+# The app now relies on explicit virtual attributes in the few models that
+# actually need `*_global_id` in forms (e.g. `Stone`, `Box`, `Place`, `Analysis`).
