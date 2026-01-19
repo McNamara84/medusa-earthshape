adaf00ee474fed1597b0cbdf9e853da17e7aa81b
McNamara <holgerehrmann@gmail.com>
2026-01-19 14:39:21 +0100

    Prevent default action in display type viewer
    Added event.preventDefault() and return false to the changeDisplay function to prevent default link behavior when switching display types.

--- app/assets/javascripts/display_type_viewer.js
+++ app/assets/javascripts/display_type_viewer.js
@@ -1,8 +1,10 @@
 (function($) {
-  var changeDisplay = function() {
+  var changeDisplay = function(event) {
+    event.preventDefault();
     var div_id = $(this).attr('href');
     $("div.display-type").children().addClass("hidden");
     $(div_id).removeClass("hidden");
+    return false;
   };
 
   $(document).on("click", ".radio-button-group", changeDisplay);
