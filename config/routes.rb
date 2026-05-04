0ea8c3c5e29d5402844be91948986542095d36d7
Holger Ehrmann <holgerehrmann@gmail.com>
2026-05-04 08:17:29 +0200

    Fixed issues from review

--- config/routes.rb
+++ config/routes.rb
@@ -88,7 +88,7 @@ Rails.application.routes.draw do
 
   resources :records, only: [:index]
 
-  resources :records, only: [:show, :destroy], format: false, constraints: { id: /[^\/]+/ } do
+  resources :records, only: [:show, :destroy], constraints: { id: /((?!\.(html$|json$|xml$|pml$|modal$)).)+/ } do
     member do
       get 'record_property' => 'records#property'
       get 'casteml'
